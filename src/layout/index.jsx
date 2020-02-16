b24c521f6d394e6411ad62b41b5bd6ae9d205384
atmasark <moroatte@gmail.com>
2020-02-16 20:20:03 +0200

    remove unnecessary div and import

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,18 +1,10 @@
 import React from "react"
-import styled, { ThemeProvider } from "styled-components"
+import { ThemeProvider } from "styled-components"
 import theme from "../styles/theme"
 import "../styles/styles.css"
 
-const Wrapper = styled.div`
-  display: flex;
-`
-
 const Layout = ({ children }) => {
-  return (
-    <Wrapper>
-      <ThemeProvider theme={theme}>{children}</ThemeProvider>
-    </Wrapper>
-  )
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>
 }
 
 export default Layout
