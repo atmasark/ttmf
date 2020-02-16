098a35d2c5d30a9f90e80b14af245acd36a6381b
atmasark <moroatte@gmail.com>
2020-02-16 20:36:00 +0200

    add slight scale&spin on logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -8,9 +8,16 @@ const colorRotate = keyframes`
   }
 `
 
+const scaleAndSpin = keyframes`
+  0% { transform: scale(1) rotate(0deg); }
+  45% { transform: scale(1.05) rotate(3deg); }
+  55% { transform: scale(1.049) rotate(2.99deg); }
+`
+
 const Image = styled.img`
-  animation: ${colorRotate} 5s linear infinite;
-  max-width: 25%;
+  animation: ${colorRotate} 5s linear infinite,
+    ${scaleAndSpin} 20s linear infinite;
+  height: 50vh;
 `
 
 export default () => <Image src={image} />
