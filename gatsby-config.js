968a7794fec58622cd9112bf872a19a577a01487
atmasark <moroatte@gmail.com>
2020-02-16 16:37:19 +0200

    change description etc

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `The Trippy Motherfuckers`,
+    description: `The trippy website for The Trippy Motherfuckers`,
+    author: `The Trippy Motherfuckers`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
