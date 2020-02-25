ac43777bf4137cc689ea6653ca61d6dd4a430913
atmasark <moroatte@gmail.com>
2020-02-25 14:01:54 +0200

    finetuning

--- src/components/Home/Background/setup.js
+++ src/components/Home/Background/setup.js
@@ -8,8 +8,8 @@ export const getSpriteInitSettings = (PIXI, i, parent) => ({
     texture: gear,
     anchor: 0.5,
     scale: {
-      x: 0.23,
-      y: 0.23,
+      x: 0.25,
+      y: 0.25,
     },
     x: (i % (Math.round(parent.width / 64) + 1)) * 64 + 32,
     y: Math.floor(i / (Math.round(parent.width / 64) + 1)) * 64 + 32,
@@ -26,12 +26,10 @@ export const getSpriteTickerSettings = (i, counters) => ({
     rotation: i % 2 ? 0.01 : -0.01,
     scale: {
       x:
-        0.35 * Math.sin(counters.scale) > 0.23 &&
-        0.35 * Math.sin(counters.scale),
+        0.4 * Math.sin(counters.scale) > 0.25 && 0.4 * Math.sin(counters.scale),
       y:
-        0.35 * Math.sin(counters.scale) > 0.23 &&
-        0.35 * Math.sin(counters.scale),
+        0.4 * Math.sin(counters.scale) > 0.25 && 0.4 * Math.sin(counters.scale),
     },
-    alpha: 0.03 + 0.0055 * Math.sin(counters.alpha),
+    alpha: 0.035 + 0.0055 * Math.sin(counters.alpha),
   },
 })
